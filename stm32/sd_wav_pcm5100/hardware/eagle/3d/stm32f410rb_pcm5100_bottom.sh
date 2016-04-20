c8e3bd3dc3e016ad684a7135fb3ef9e543152a4b
mtiutiu <mtiutiu@softvision.ro>
2016-04-20 10:11:40 +0300

    [sd_wav_pcm5100] some final pcb touches

---
+++ stm32/sd_wav_pcm5100/hardware/eagle/3d/stm32f410rb_pcm5100_bottom.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+"povray" +W1280 +H960 +A0.3 +FN +L"/usr/share/fonts" +L"/home/mtiutiu/Util/eagle/latest/eagle3d/ulp//../povray" +O"/home/mtiutiu/Work/ARM_Playground/stm32/sd_wav_pcm5100/hardware/eagle/3d/stm32f410rb_pcm5100_bottom.png" /home/mtiutiu/Work/ARM_Playground/stm32/sd_wav_pcm5100/hardware/eagle/3d/stm32f410rb_pcm5100_bottom.pov 
